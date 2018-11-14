e92ca784c1018cca06dce904be10c322b7d6473d
AKhan139 <khana17@mail.wlu.edu>
2018-11-14 12:57:04 -0600

    add main class

---
+++ java/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int choice;
+        String selection;
+        String[] questions = new String[10];
+    }
+    
+
+}
