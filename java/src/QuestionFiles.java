3f0a68af2b776f1dbeb3fcf5b43b6365ffb6743a
Caleigh Wells <caleigh-wells@Caleighs-MacBook-Air.local>
2018-11-30 00:58:35 -0600

    add file handling

---
+++ java/src/QuestionFiles.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class QuestionFiles {
+
+    public static void main(String [] args) {
+
+        // The name of the file to open.
+        String fileName = "design-docs/questions.txt";
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader(fileName);
+
+            // Wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Close file.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" + fileName + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '" + fileName + "'");
+        }
+    }
+}
